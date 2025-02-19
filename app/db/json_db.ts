4025741b93575d8f637ebfd7150773364de8718d
Vladtrif <vova.trifonov@hotmail.com>
2025-02-18 19:42:03 -0800

    Copied code from main

--- app/db/json_db.ts
+++ app/db/json_db.ts
@@ -1,4 +1,10 @@
 import {DB, Workout, Exercise_List, Exercise_Hist, Muscle_Group} from './Types'
+import * as FileSystem from 'expo-file-system';
+
+const workout_path: string = ""
+const exercise_list_path: string = ""
+const exercise_hist_path: string = ""
+
 // implements interface for json
 export class json_db implements DB {
     
---
+++ src/styles/themes/colors.ts
@@ -0,0 +1,12 @@
+// Colors to be used throughout the app
+
+const colors = {
+    BACKGROUND_COLOR: '#1a002e',
+    BUTTON_COLOR: '#e6d5ff',
+    BUTTON_TEXT: '#4a0072',
+    HEADER_COLOR: '#1a002e',
+    HEADER_TEXT: '#4a0072',
+    WHITE: '#FFFFFF',
+};
+
+export default colors;
