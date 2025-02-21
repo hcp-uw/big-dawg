f1f2668c9b753cae88bee130f28073d3874e09e2
Vladimirtrif <vova.trifonov@hotmail.com>
2025-02-20 16:39:06 -0800

    drafted all workout methods for db

--- app/db/json_db.ts
+++ app/db/json_db.ts
@@ -3,6 +3,8 @@ import * as FS from 'expo-file-system';
 
 const data_dir: string = FS.documentDirectory + '.big-dawg/data/'
 
+//type Workout_File = (Workout|undefined)[]
+
 // implements interface for json
 export class json_db implements DB {
     
@@ -11,12 +13,12 @@ export class json_db implements DB {
       const file_name: string  =  (w.Date.getMonth() + 1) + "_" + w.Date.getFullYear() + ".json"
       const uri: string = data_dir + file_name;
       var workout_exists: boolean = await createFile(file_name)
-      var content: Workout[]
+      var content: (Workout|undefined)[]
       if (workout_exists) {
         content = JSON.parse(await FS.readAsStringAsync(uri))
-        workout_exists = !(content[w.Date.getDate() - 1] == undefined)
+        workout_exists = !(content[w.Date.getDate() - 1] === undefined)
       } else {
-        content = new Array<Workout>(31)
+        content = new Array<Workout|undefined>(31).fill(undefined)
       }
       content[w.Date.getDate() - 1] = w;
       await FS.writeAsStringAsync(uri, JSON.stringify(content))
@@ -24,15 +26,31 @@ export class json_db implements DB {
     }
 
     async getWorkout (date: Date): Promise<Workout | null> {
-        
+      const file_name: string  =  (date.getMonth() + 1) + "_" + date.getFullYear() + ".json"
+      const uri: string = data_dir + file_name;
+      if (!(await checkFile(file_name))) {
+        return null;
+      }
+      var content: Workout[] = JSON.parse(await FS.readAsStringAsync(uri))
+      var result = content[date.getDate() - 1]
+      if (result === undefined) { return null }
+      return result
     }
 
     async deleteWorkout (date: Date): Promise<boolean> {
-
+      const file_name: string  =  (date.getMonth() + 1) + "_" + date.getFullYear() + ".json"
+      const uri: string = data_dir + file_name;
+      if (!(await checkFile(file_name))) {
+        return false;
+      }
+      var content: (Workout|undefined)[] = JSON.parse(await FS.readAsStringAsync(uri))
+      const result: boolean  = !(content[date.getDate() - 1] === undefined)
+      content[date.getDate() - 1] = undefined;
+      return result;
     }
 
     async getExerciseList (): Promise<Exercise_List> {
-
+      
     }
 
     async getExerciseHistory (ex_name: string): Promise<Exercise_Hist> {
