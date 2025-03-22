0f659b9b928d9545f9307ab979411b4bbb63f77e
Vladimirtrif <vova.trifonov@hotmail.com>
2025-03-21 21:47:19 -0700

    Fixed folder structure

--- app/db/__tests__/JsonDB-Exercises-test.tsx
+++ app/db/__tests__/JsonDB-Exercises-test.tsx
@@ -1,72 +1,74 @@
-import {DB, Muscle_Group, Exercise, Exercise_List, Set, Exercise_Hist, Workout} from '../Types'
-import{setupTest} from './Testing-utils'
+import { DB, Muscle_Group, Exercise, Exercise_List, Set, Exercise_Hist, Workout } from '../Types'
+import { setupTest } from '../Testing-utils'
 
 describe('json_db Exercise Tests', () => {
-    afterEach(() => {
-      console.log("Test output end")
-      // resets mocked funcs created with spy on (used for mocked json_db funcs)
-      jest.resetModules()
-      jest.clearAllMocks()
-    })
-
-    it('saveExercise_alreadyExists', async () => {
-      console.log("Test saveExercise_alreadyExists output begin")
-      const ex1: Exercise = {
-        Exercise_Name: "Bench",
-        Muscle_Group: "Chest",
-        Comment: "Barbell"
-      }
-      let {db} = setupTest() 
-      await expect(db.saveExercise(ex1)).resolves.toBe(false)
-    })
-
-    it('saveExercise_emptyExerciseList', async () => {
-      console.log("Test saveExercise_emptyExerciseList output begin")
-      // constants / test data
-      const ex1: Exercise = {
-        Exercise_Name: "Bench",
-        Muscle_Group: "Chest",
-        Comment: "Barbell"
-      }
-      const el: Exercise_List = {
-        Chest:[{Exercise_Name: "Bench", Muscle_Group: "Chest", Comment: "Barbell"}],
-        Back:[], Legs:[],Triceps:[], Biceps:[], Shoulders:[]
-      }
-      const w1 = {uri: ".big-dawg/data/Bench.json", content: "{\"Exercise_Name\":\"Bench\",\"Hist\":[]}"}
-      const w2 = {uri: ".big-dawg/data/Exercise_List.json",
-        content: JSON.stringify(el)
-      }
-
-      // change mock implementation setup
-      let {db} = setupTest({file_exists: false, expected_wContents: [w1, w2]}) 
-      // test
-      await expect(db.saveExercise(ex1)).resolves.toBe(true)
-    })
-
-    it('saveExercise_updatedExerciseList', async () => {
-      console.log("Test saveExercise_updatedExerciseList output begin")
-      // constants/ test data
-      const ex1: Exercise = {
-        Exercise_Name: "Squat",
-        Muscle_Group: "Legs",
-        Comment: "Dope"
-      }
-      const ex2: Exercise = {
-        Exercise_Name: "Bench",
-        Muscle_Group: "Chest",
-        Comment: "Barbell"
-      }
-      const el: Exercise_List = {Chest:[ex2],Back:[], Legs:[ex1], Triceps:[], Biceps:[], Shoulders:[]}
-      const w1 = {uri: ".big-dawg/data/Bench.json", content: "{\"Exercise_Name\":\"Bench\",\"Hist\":[]}"}
-      const w2 = {uri: ".big-dawg/data/Exercise_List.json",
-        content: JSON.stringify(el)
-      }
-       //  change mock implementation setup
-      let {db} = setupTest({file_exists: false, expected_wContents: [w1, w2]}) 
-      jest.spyOn(db, 'getExerciseList').mockImplementation(() => Promise.resolve(
-        { Chest:[],Back:[],Legs:[ex1], Triceps:[], Biceps:[],Shoulders:[] }
-      ))
-      // test
-      await expect(db.saveExercise(ex2)).resolves.toBe(true)
-    })
+  afterEach(() => {
+    console.log("Test output end")
+    // resets mocked funcs created with spy on (used for mocked json_db funcs)
+    jest.resetModules()
+    jest.clearAllMocks()
   })
+
+  it('saveExercise_alreadyExists', async () => {
+    console.log("Test saveExercise_alreadyExists output begin")
+    const ex1: Exercise = {
+      Exercise_Name: "Bench",
+      Muscle_Group: "Chest",
+      Comment: "Barbell"
+    }
+    let { db } = setupTest()
+    await expect(db.saveExercise(ex1)).resolves.toBe(false)
+  })
+
+  it('saveExercise_emptyExerciseList', async () => {
+    console.log("Test saveExercise_emptyExerciseList output begin")
+    // constants / test data
+    const ex1: Exercise = {
+      Exercise_Name: "Bench",
+      Muscle_Group: "Chest",
+      Comment: "Barbell"
+    }
+    const el: Exercise_List = {
+      Chest: [{ Exercise_Name: "Bench", Muscle_Group: "Chest", Comment: "Barbell" }],
+      Back: [], Legs: [], Triceps: [], Biceps: [], Shoulders: []
+    }
+    const w1 = { uri: ".big-dawg/data/Bench.json", content: "{\"Exercise_Name\":\"Bench\",\"Hist\":[]}" }
+    const w2 = {
+      uri: ".big-dawg/data/Exercise_List.json",
+      content: JSON.stringify(el)
+    }
+
+    // change mock implementation setup
+    let { db } = setupTest({ file_exists: false, expected_wContents: [w1, w2] })
+    // test
+    await expect(db.saveExercise(ex1)).resolves.toBe(true)
+  })
+
+  it('saveExercise_updatedExerciseList', async () => {
+    console.log("Test saveExercise_updatedExerciseList output begin")
+    // constants/ test data
+    const ex1: Exercise = {
+      Exercise_Name: "Squat",
+      Muscle_Group: "Legs",
+      Comment: "Dope"
+    }
+    const ex2: Exercise = {
+      Exercise_Name: "Bench",
+      Muscle_Group: "Chest",
+      Comment: "Barbell"
+    }
+    const el: Exercise_List = { Chest: [ex2], Back: [], Legs: [ex1], Triceps: [], Biceps: [], Shoulders: [] }
+    const w1 = { uri: ".big-dawg/data/Bench.json", content: "{\"Exercise_Name\":\"Bench\",\"Hist\":[]}" }
+    const w2 = {
+      uri: ".big-dawg/data/Exercise_List.json",
+      content: JSON.stringify(el)
+    }
+    //  change mock implementation setup
+    let { db } = setupTest({ file_exists: false, expected_wContents: [w1, w2] })
+    jest.spyOn(db, 'getExerciseList').mockImplementation(() => Promise.resolve(
+      { Chest: [], Back: [], Legs: [ex1], Triceps: [], Biceps: [], Shoulders: [] }
+    ))
+    // test
+    await expect(db.saveExercise(ex2)).resolves.toBe(true)
+  })
+})
--- app/db/__tests__/JsonDB-Workouts-test.tsx
+++ app/db/__tests__/JsonDB-Workouts-test.tsx
@@ -1,50 +1,50 @@
-import {DB, Muscle_Group, Exercise, Exercise_List, Set, Exercise_Hist, Workout} from '../Types'
-import{setupTest} from './Testing-utils'
+import { DB, Muscle_Group, Exercise, Exercise_List, Set, Exercise_Hist, Workout } from '../Types'
+import { setupTest } from '../Testing-utils'
 
 describe('json_db Workout Tests', () => {
-    afterEach(() => {
-        console.log("Test output end")
-        // resets mocked funcs created with spy on (used for mocked json_db funcs)
-        jest.resetModules()
-        jest.clearAllMocks()
-      })
-      it('getWorkout_doesn\'tExist', async () => {
-        console.log("Test getWorkout_doesn\'tExist output begin")
-        let {db} = setupTest({file_exists: false})
-        await expect(db.getWorkout(new Date())).resolves.toBe(null)
-      })
-      it('getWorkout_exists ', async () => {
-        console.log("Test getWorkout_exists output begin")
-        const set1: Set = {
+  afterEach(() => {
+    console.log("Test output end")
+    // resets mocked funcs created with spy on (used for mocked json_db funcs)
+    jest.resetModules()
+    jest.clearAllMocks()
+  })
+  it('getWorkout_doesn\'tExist', async () => {
+    console.log("Test getWorkout_doesn\'tExist output begin")
+    let { db } = setupTest({ file_exists: false })
+    await expect(db.getWorkout(new Date())).resolves.toBe(null)
+  })
+  it('getWorkout_exists ', async () => {
+    console.log("Test getWorkout_exists output begin")
+    const set1: Set = {
 
-        }
-        const set2: Set = {
+    }
+    const set2: Set = {
 
-        }
-        const w: Workout = {
-            Date: new Date(),
-            TimeStarted: 10n,
-            TimeEnded: 12n,
-            Sets : [set1, set1, set2, set2, set1],
-            WorkoutComment: "Good workout"
-        }
-        let {db} = setupTest({file_exists: false})
-        await expect(db.getWorkout(new Date())).resolves.toBe(null)
-      })
-      it('saveWorkout ', async () => {
-        console.log("Test saveExercise_alreadyExists output begin")
-      })
-      it('addToExerciseHist ', async () => {
-        console.log("Test saveExercise_alreadyExists output begin")
-      })
-      it('deleteWorkout ', async () => {
-        console.log("Test saveExercise_alreadyExists output begin")
-      })
-      it('deleteWorkout ', async () => {
-        console.log("Test saveExercise_alreadyExists output begin")
-      })
-      it('deleteFromExerciseHist ', async () => {
-        console.log("Test saveExercise_alreadyExists output begin")
-      })
+    }
+    const w: Workout = {
+      Date: new Date(),
+      TimeStarted: 10n,
+      TimeEnded: 12n,
+      Sets: [set1, set1, set2, set2, set1],
+      WorkoutComment: "Good workout"
+    }
+    let { db } = setupTest({ file_exists: false })
+    await expect(db.getWorkout(new Date())).resolves.toBe(null)
+  })
+  it('saveWorkout ', async () => {
+    console.log("Test saveExercise_alreadyExists output begin")
+  })
+  it('addToExerciseHist ', async () => {
+    console.log("Test saveExercise_alreadyExists output begin")
+  })
+  it('deleteWorkout ', async () => {
+    console.log("Test saveExercise_alreadyExists output begin")
+  })
+  it('deleteWorkout ', async () => {
+    console.log("Test saveExercise_alreadyExists output begin")
+  })
+  it('deleteFromExerciseHist ', async () => {
+    console.log("Test saveExercise_alreadyExists output begin")
+  })
 
 })
