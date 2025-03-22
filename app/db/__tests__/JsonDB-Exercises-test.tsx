d4e64a41c6e14746770fcfa20aae8d53138327e0
Vladimirtrif <vova.trifonov@hotmail.com>
2025-03-21 23:17:57 -0700

    Fixes and refactor

--- app/db/__tests__/JsonDB-Exercises-test.tsx
+++ app/db/__tests__/JsonDB-Exercises-test.tsx
@@ -1,6 +1,24 @@
 import { DB, Muscle_Group, Exercise, Exercise_List, Set, Exercise_Hist, Workout } from '../Types'
 import { setupTest } from '../Testing-utils'
 
+// consts for tests
+const ex1: Exercise = {
+  Exercise_Name: "Bench",
+  Muscle_Group: "Chest",
+  Comment: "Barbell"
+}
+const ex2: Exercise = {
+  Exercise_Name: "Bench",
+  Muscle_Group: "Chest",
+  Comment: "Barbell"
+}
+const el: Exercise_List = {
+  Chest: [ex1], Back: [], Legs: [], Triceps: [], Biceps: [], Shoulders: []
+}
+
+const el2: Exercise_List = {
+  Chest: [ex2], Back: [], Legs: [ex1], Triceps: [], Biceps: [], Shoulders: []
+}
 describe('json_db Exercise Tests', () => {
   afterEach(() => {
     console.log("Test output end")
@@ -11,33 +29,17 @@ describe('json_db Exercise Tests', () => {
 
   it('saveExercise_alreadyExists', async () => {
     console.log("Test saveExercise_alreadyExists output begin")
-    const ex1: Exercise = {
-      Exercise_Name: "Bench",
-      Muscle_Group: "Chest",
-      Comment: "Barbell"
-    }
     let { db } = setupTest()
     await expect(db.saveExercise(ex1)).resolves.toBe(false)
   })
 
   it('saveExercise_emptyExerciseList', async () => {
     console.log("Test saveExercise_emptyExerciseList output begin")
-    // constants / test data
-    const ex1: Exercise = {
-      Exercise_Name: "Bench",
-      Muscle_Group: "Chest",
-      Comment: "Barbell"
-    }
-    const el: Exercise_List = {
-      Chest: [{ Exercise_Name: "Bench", Muscle_Group: "Chest", Comment: "Barbell" }],
-      Back: [], Legs: [], Triceps: [], Biceps: [], Shoulders: []
-    }
     const w1 = { uri: ".big-dawg/data/Bench.json", content: "{\"Exercise_Name\":\"Bench\",\"Hist\":[]}" }
     const w2 = {
       uri: ".big-dawg/data/Exercise_List.json",
       content: JSON.stringify(el)
     }
-
     // change mock implementation setup
     let { db } = setupTest({ file_exists: false, expected_wContents: [w1, w2] })
     // test
@@ -46,22 +48,10 @@ describe('json_db Exercise Tests', () => {
 
   it('saveExercise_updatedExerciseList', async () => {
     console.log("Test saveExercise_updatedExerciseList output begin")
-    // constants/ test data
-    const ex1: Exercise = {
-      Exercise_Name: "Squat",
-      Muscle_Group: "Legs",
-      Comment: "Dope"
-    }
-    const ex2: Exercise = {
-      Exercise_Name: "Bench",
-      Muscle_Group: "Chest",
-      Comment: "Barbell"
-    }
-    const el: Exercise_List = { Chest: [ex2], Back: [], Legs: [ex1], Triceps: [], Biceps: [], Shoulders: [] }
     const w1 = { uri: ".big-dawg/data/Bench.json", content: "{\"Exercise_Name\":\"Bench\",\"Hist\":[]}" }
     const w2 = {
       uri: ".big-dawg/data/Exercise_List.json",
-      content: JSON.stringify(el)
+      content: JSON.stringify(el2)
     }
     //  change mock implementation setup
     let { db } = setupTest({ file_exists: false, expected_wContents: [w1, w2] })
--- app/db/__tests__/JsonDB-Workouts-test.tsx
+++ app/db/__tests__/JsonDB-Workouts-test.tsx
@@ -1,6 +1,27 @@
 import { DB, Muscle_Group, Exercise, Exercise_List, Set, Exercise_Hist, Workout } from '../Types'
 import { setupTest } from '../Testing-utils'
 
+// consts for tests
+const set1: Set = {
+  Exercise_Name: "Bench",
+  Weight: 225,
+  Reps: 3,
+  Comment: null
+}
+const set2: Set = {
+  Exercise_Name: "Squat",
+  Weight: 315.5,
+  Reps: 3,
+  Comment: "PR!"
+}
+const w: Workout = {
+  Date: new Date(),
+  TimeStarted: 10,
+  TimeEnded: 12,
+  Sets: [set1, set1, set2],
+  WorkoutComment: "Good workout"
+}
+
 describe('json_db Workout Tests', () => {
   afterEach(() => {
     console.log("Test output end")
@@ -22,29 +43,21 @@ describe('json_db Workout Tests', () => {
       Sets: [],
       WorkoutComment: "Good workout"
     }
-    let c: string = JSON.stringify([w, null, w])
-    let { db } = setupTest({ file_exists: true, expected_rContents: [c] })
+    let { db } = setupTest({ file_exists: true, expected_rContents: [JSON.stringify([w, null, w])] })
     await expect(db.getWorkout(new Date(2025, 1, 2))).resolves.toBe(null)
   })
   it('getWorkout_exists', async () => {
-    /*console.log("Test getWorkout_exists output begin")
-    const set1: Set = {
-
-    }
-    const set2: Set = {
-
-    }
-    const w: Workout = {
-      Date: new Date(),
-      TimeStarted: 10n,
-      TimeEnded: 12n,
-      Sets: [set1, set1, set2, set2, set1],
-      WorkoutComment: "Good workout"
-    }
-    let { db } = setupTest({ file_exists: true, expected_rContents: [JSON.stringify(w)] })
-    await expect(db.getWorkout(new Date())).resolves.toBe(null)*/
+    console.log("Test getWorkout_exists output begin")
+    let { db } = setupTest({ file_exists: true, expected_rContents: [JSON.stringify([null, null, null, w])] })
+    await expect(db.getWorkout(new Date(2025, 1, 4))).resolves.toStrictEqual(w)
   })
-  it('saveWorkout ', async () => {
+  it('saveWorkout_invalidExercise', async () => {
+
+  })
+  it('saveWorkout_newMonth', async () => {
+    console.log("Test saveExercise_alreadyExists output begin")
+  })
+  it('saveWorkout_updatingMMonth', async () => {
     console.log("Test saveExercise_alreadyExists output begin")
   })
   it('addToExerciseHist ', async () => {
